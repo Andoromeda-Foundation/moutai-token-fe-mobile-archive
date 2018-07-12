cb385d6792b8b38c59346da2fe0527c74ad2d86b
Frank Wei <frank@frankwei.xyz>
2018-07-12 17:11:58 +0800

    feat: the very first footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,12 @@
 <template>
   <div class="footer">
-    Footer
+    <footer class="footer">
+      <div class="content has-text-centered">
+        <h1 class="title">白酒银行</h1>
+        <p>
+          The Bank of Liquor All Right Reserved.
+        </p>
+      </div>
+    </footer>
   </div>
 </template>
