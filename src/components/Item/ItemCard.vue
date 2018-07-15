78c4fe103d1ca905d579f4c3c8e0bdf3bf5dd4dc
LookForwardPersistence <fqhua_@sina.cn>
2018-07-15 22:05:18 +0800

    Home UI

--- src/components/Item/ItemCard.vue
+++ src/components/Item/ItemCard.vue
@@ -3,6 +3,7 @@
   <div class="card-image" style="margin-top: 1rem">
     <figure class="image is-4by4">
        <img :src="props.coverFileDownloadUrl" alt="商品" class="item-img">
+      <span class="img-font"><span style="font-size: 0.8rem">¥</span>{{!props.nextPrice?props.currentPrice:props.nextPrice}}</span>
       <!--<img src="https://ws3.sinaimg.cn/large/006tKfTcgy1ft7ajz8j0tj306404lq2s.jpg" alt="商品" class="item-img">-->
     </figure>
   </div>
@@ -47,4 +48,18 @@
   color: #3EC170;
   float: right;
 }
+  .img-font{
+    position: absolute;
+    background: #3F51B5;
+    color: #ffff;
+    top: 0.7rem;
+    right: 0.3rem;
+    font-weight: 600;
+    width: 4.5rem;
+    align-contenet: center;
+    marigin: 1rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
 </style>
