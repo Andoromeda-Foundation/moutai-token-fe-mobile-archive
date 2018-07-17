36d9031ad445b1d8a804dbc7389d22d7b0ada3e6
Frank Wei <frank@frankwei.xyz>
2018-07-17 10:08:38 +0800

    feat: abstraction for the tabbar

---
+++ src/components/GeneralTabBar.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="footer">
+    <mt-tabbar v-model="selected"
+    :fixed="true">
+      <mt-tab-item v-for="menu in menus" :key="menu.name" id="menu.name" @click.native="menuTab(menu.location)">
+        <img slot="icon" :src="menu.logo">
+        {{menu.name}}
+      </mt-tab-item>
+    </mt-tabbar>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+export default {
+  name: "GeneralNavigateTabBar",
+  props: ["menus"],
+  data: () => ({
+    selected: 0
+  }),
+  computed: {
+    ...mapGetters({
+      token: "getToken"
+    })
+  },
+  methods: {
+    menuTab(name) {
+      console.log(name);
+      if (name.indexOf("/User") > -1) {
+        if (!this.token) {
+          Toast("请先登录");
+          this.$router.push("/Login");
+          return;
+        } else {
+          this.$router.push(name);
+        }
+      } else {
+        this.$router.push(name);
+      }
+    }
+  }
+};
+</script>
